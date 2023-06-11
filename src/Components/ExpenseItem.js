f4b4d982d160f25ac4d08c824bdd3d25157fba23
Krushna1621 <shindekrushna825@gmail.com>
2023-06-11 22:25:26 +0530

    split the components in smaller part

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,19 +1,34 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 const ExpenseItem=(props)=>{
     // const expenseDate=new Date(2021,2,28);
     // const expenseTitle='Car Insurance';
     // const expenseAmount='430'
     // const LocationOfExpenditure='Amaravati'
+
+
+    // const month=props.date.toLocaleString('en-US',{month:'long'});
+    // const day=props.date.toLocaleString('en-US',{day:'2-digit'});
+    // const year=props.date.getFullYear()
+
+    
     return(
         <div className='expense-item '>
-            <div>{props.date.toISOString()}</div>
+            {/* <div>{props.date.toISOString()}</div> */}
+            {/* <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div> */}
+            <ExpenseDate date={props.date}></ExpenseDate>
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
             {/* <h2>{LocationOfExpenditure}</h2> */}
+            <div className='expense-item__price'>${props.amount}</div>
         </div>
-        <div className='expense-item__price'>${props.amount}</div>
+        
         </div>
-         
+          
     )
 
 }
